8f169247aa2b9c964a347b96f05b30b90a2d5d92
Atsushi Ishida <gipcompany@gmail.com>
2020-12-19 23:38:53 +0900

    just update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount((previousCount) => previousCount + 1);
+  };
+
+  return (
+    <div>
+      count: {count} <button onClick={increment}>click me</button>
+    </div>
+  );
 }
 
 export default App;
